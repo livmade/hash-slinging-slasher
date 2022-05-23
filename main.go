9db3c92d79f1f86131d09a5687eae3cd8edfb8fb
Olivia Smith <oliviansmith@outlook.com>
2022-05-23 03:59:09 -0400

    Started HTML

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
-//initializing project
+//Test HTML
 
 import (
-	"fmt"
+	"html/template"
+	"net/http"
+	//"log"
+	//"fmt"
+	//"os"
 )
 
+var t *template.Template
+
+type usrID struct {
+	Name string
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	var err error
+	t, err = template.ParseFiles("login.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/", loginPage)
+	http.ListenAndServe(":3000", nil)
+}
+
+func loginPage(w http.ResponseWriter, r *http.Request) {
+
+	uN := usrID{"Olivia Smith"}
+	err := t.Execute(w, uN)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
